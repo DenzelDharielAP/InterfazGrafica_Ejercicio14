6e4857924c237269bf3b3d93f9f5293b77523be4
Denzel Dhariel Alvarez Perez <denzelpoli@outlook.es>
2024-03-07 20:32:00 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace InterfazGrafica_Ejercicio15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAsignatura = new System.Windows.Forms.TextBox();
+            this.rbtnPresencial = new System.Windows.Forms.RadioButton();
+            this.txtProfesor = new System.Windows.Forms.TextBox();
+            this.rbtnVirtual = new System.Windows.Forms.RadioButton();
+            this.lblAsignatura = new System.Windows.Forms.Label();
+            this.lblProfesor = new System.Windows.Forms.Label();
+            this.lblModalidad = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAsignatura
+            // 
+            this.txtAsignatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsignatura.Location = new System.Drawing.Point(203, 120);
+            this.txtAsignatura.Name = "txtAsignatura";
+            this.txtAsignatura.Size = new System.Drawing.Size(128, 22);
+            this.txtAsignatura.TabIndex = 0;
+            // 
+            // rbtnPresencial
+            // 
+            this.rbtnPresencial.AutoSize = true;
+            this.rbtnPresencial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnPresencial.Location = new System.Drawing.Point(240, 60);
+            this.rbtnPresencial.Name = "rbtnPresencial";
+            this.rbtnPresencial.Size = new System.Drawing.Size(89, 20);
+            this.rbtnPresencial.TabIndex = 1;
+            this.rbtnPresencial.TabStop = true;
+            this.rbtnPresencial.Text = "Presencial";
+            this.rbtnPresencial.UseVisualStyleBackColor = true;
+            // 
+            // txtProfesor
+            // 
+            this.txtProfesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProfesor.Location = new System.Drawing.Point(415, 120);
+            this.txtProfesor.Name = "txtProfesor";
+            this.txtProfesor.Size = new System.Drawing.Size(123, 22);
+            this.txtProfesor.TabIndex = 2;
+            // 
+            // rbtnVirtual
+            // 
+            this.rbtnVirtual.AutoSize = true;
+            this.rbtnVirtual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnVirtual.Location = new System.Drawing.Point(428, 60);
+            this.rbtnVirtual.Name = "rbtnVirtual";
+            this.rbtnVirtual.Size = new System.Drawing.Size(62, 20);
+            this.rbtnVirtual.TabIndex = 3;
+            this.rbtnVirtual.TabStop = true;
+            this.rbtnVirtual.Text = "Virtual";
+            this.rbtnVirtual.UseVisualStyleBackColor = true;
+            // 
+            // lblAsignatura
+            // 
+            this.lblAsignatura.AutoSize = true;
+            this.lblAsignatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsignatura.Location = new System.Drawing.Point(331, 165);
+            this.lblAsignatura.Name = "lblAsignatura";
+            this.lblAsignatura.Size = new System.Drawing.Size(0, 18);
+            this.lblAsignatura.TabIndex = 4;
+            // 
+            // lblProfesor
+            // 
+            this.lblProfesor.AutoSize = true;
+            this.lblProfesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProfesor.Location = new System.Drawing.Point(331, 207);
+            this.lblProfesor.Name = "lblProfesor";
+            this.lblProfesor.Size = new System.Drawing.Size(0, 18);
+            this.lblProfesor.TabIndex = 5;
+            // 
+            // lblModalidad
+            // 
+            this.lblModalidad.AutoSize = true;
+            this.lblModalidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModalidad.Location = new System.Drawing.Point(331, 249);
+            this.lblModalidad.Name = "lblModalidad";
+            this.lblModalidad.Size = new System.Drawing.Size(0, 18);
+            this.lblModalidad.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Asignatura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(444, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Profesor";
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.BackColor = System.Drawing.Color.DarkOrchid;
+            this.btnProcesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnProcesar.Location = new System.Drawing.Point(615, 149);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(103, 31);
+            this.btnProcesar.TabIndex = 9;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = false;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.DarkOrchid;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLimpiar.Location = new System.Drawing.Point(615, 204);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(103, 31);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.DarkOrchid;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCerrar.Location = new System.Drawing.Point(615, 257);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(103, 31);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblModalidad);
+            this.Controls.Add(this.lblProfesor);
+            this.Controls.Add(this.lblAsignatura);
+            this.Controls.Add(this.rbtnVirtual);
+            this.Controls.Add(this.txtProfesor);
+            this.Controls.Add(this.rbtnPresencial);
+            this.Controls.Add(this.txtAsignatura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAsignatura;
+        private System.Windows.Forms.RadioButton rbtnPresencial;
+        private System.Windows.Forms.TextBox txtProfesor;
+        private System.Windows.Forms.RadioButton rbtnVirtual;
+        private System.Windows.Forms.Label lblAsignatura;
+        private System.Windows.Forms.Label lblProfesor;
+        private System.Windows.Forms.Label lblModalidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazGrafica_Ejercicio15
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            string asignatura, profesor;
+
+            asignatura = txtAsignatura.Text;
+            profesor = txtProfesor.Text;
+
+            if (rbtnPresencial.Checked)
+            {
+                lblAsignatura.Text = asignatura;
+                lblProfesor.Text = profesor;
+                lblModalidad.Text = "Modalidad presencial";
+            }
+
+            else 
+            {
+                lblAsignatura.Text = asignatura;
+                lblProfesor.Text = profesor;
+                lblModalidad.Text = "Modalidad virtual";
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtAsignatura.Clear();
+            txtProfesor.Clear();
+
+            rbtnPresencial.Checked = false;
+            rbtnVirtual.Checked = false;
+
+            lblAsignatura.ResetText();
+            lblModalidad.ResetText();
+            lblProfesor.ResetText();
+
+            txtAsignatura.Focus();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult  respuesta = (MessageBox.Show("¿Desea salir de la aplicación?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question));
+
+            if (respuesta == DialogResult.Yes)
+            {
+                Close();
+
+            }
+
+            else { txtAsignatura.Focus(); }
+        }
+    }
+}
